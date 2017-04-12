52062d195ea0782240455d0687d2bf80d13cdbae
AB48917 <rakeshupadhayay88@gmail.com>
2017-04-12 20:33:56 +0530

    added necessary library classes and helpers

---
+++ src/main/java/ctl/graphwalker/pmt/modelImpl/LoginImpl.java
@@ -0,0 +1,101 @@
+package ctl.graphwalker.pmt.modelImpl;
+
+import org.graphwalker.core.machine.ExecutionContext;
+import org.graphwalker.java.annotation.AfterElement;
+import org.graphwalker.java.annotation.AfterExecution;
+import org.graphwalker.java.annotation.BeforeElement;
+import org.graphwalker.java.annotation.BeforeExecution;
+import org.graphwalker.java.annotation.GraphWalker;
+
+
+
+import ctl.graphwalker.pmt.Login;
+import ctl.graphwalker.pmt.helper.Helper;
+
+
+
+@GraphWalker(value = "random(edge_coverage(100))", start = "e_StartClient")
+ 
+public class LoginImpl extends ExecutionContext implements Login {
+
+	public void v_Home() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void e_ToggleRememberMe() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void e_ValidPremiumCredentials() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void v_applicationDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void e_Logout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void v_LoginPrompted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void e_StartClient() {
+		Helper.getInstance().get("http://localhost:9966/petclinic/");
+		
+	}
+
+	public void e_Close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void e_Init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void e_Exit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void e_InvalidCredentials() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@BeforeExecution
+    public void setup() {
+        System.out.println("PetClinic: Any setup steps happens here. " +
+                           "The annotation @BeforeExecution makes sure that before any elements in the " +
+                           "model is called, this method is called first");
+        Helper.setup();
+    }
+
+    @AfterExecution
+    public void cleanup() {
+        System.out.println("PetClinic: Any cleanup  steps happens here. " +
+                           "The annotation @AfterExecution makes sure that after the test is done, " +
+                           "this method is called last.");
+        Helper.tearDown();
+    }
+
+    @BeforeElement
+    public void printBeforeElement() {
+        System.out.println("Before element " + getCurrentElement().getName());
+    }
+
+    @AfterElement
+    public void printAfterElement() {
+        System.out.println("After element " + getCurrentElement().getName());
+    }
+}
