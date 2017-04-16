5092e13a30299751c1fffc1076879556399c010e
AB48917 <rakeshupadhayay88@gmail.com>
2017-04-16 16:42:40 +0530

    added graphs for Planning trigger and Application update screen

---
+++ src/main/java/ctl/graphwalker/pmt/modelImpl/ApplicationUpdateImpl.java
@@ -0,0 +1,81 @@
+package ctl.graphwalker.pmt.modelImpl;
+
+import org.graphwalker.core.machine.ExecutionContext;
+
+import ctl.graphwalker.pmt.ApplicationUpdate;
+
+public class ApplicationUpdateImpl extends ExecutionContext implements ApplicationUpdate {
+
+	@Override
+	public void v_Home() {
+		// TODO describe home application
+		
+	}
+
+	@Override
+	public void v_ProjectDetails() {
+		// TODO verify project detail screen
+		
+	}
+
+	@Override
+	public void e_UpdateAppStatus() {
+		// TODO click on update folder link, update end date and submit
+		
+	}
+
+	@Override
+	public void e_SaveAppStatus() {
+		// TODO fill the data start date and end date and submit.
+		
+	}
+
+	@Override
+	public void e_PlanningTrigger() {
+		// TODO click on PlanningTrigger component on left side of screen.
+		
+	}
+
+	@Override
+	public void e_ProjectDetailsLoc() {
+		// TODO click the project detail folder link
+		
+	}
+
+	@Override
+	public void v_ApplicationStatusUpdate() {
+		// TODO verify updated application status updated
+		
+	}
+
+	@Override
+	public void v_Application() {
+		// TODO verify application screen
+		
+	}
+
+	@Override
+	public void e_DeleteAppStatus() {
+		// TODO A click on delete entity and handle the popup -click ok.
+		
+	}
+
+	@Override
+	public void v_PlanningTrigger() {
+		// TODO verify planning trigger screen
+	}
+
+	@Override
+	public void e_ApplicationDetail() {
+		// TODO click on the application link
+		
+	}
+
+	@Override
+	public void e_AddAnotherProject() {
+		// TODO populate the other project and handle the submit
+		
+	}
+
+	
+}
---
+++ src/main/java/ctl/graphwalker/pmt/modelImpl/PlanningTriggerImpl.java
@@ -0,0 +1,41 @@
+package ctl.graphwalker.pmt.modelImpl;
+
+import org.graphwalker.core.machine.ExecutionContext;
+
+
+import ctl.graphwalker.pmt.PlanningTrigger;
+
+public class PlanningTriggerImpl extends ExecutionContext implements PlanningTrigger {
+
+	@Override
+	public void e_CorrectData() {
+		// TODO enter correct data and submit
+		
+	}
+
+	@Override
+	public void e_IncorrectData() {
+		// TODO dont enter data and and click submit
+		
+	}
+	@Override
+	public void v_IncorrectData() {
+		// TODO  handle the screen popup
+		
+	}
+
+	@Override
+	public void v_PlanningTrigger() {
+		// TODO Click the planning trigger link
+		
+	}
+
+	@Override
+	public void v_CorrectData() {
+		// TODO verify record saved successfully
+		
+	}
+
+	
+	
+}
